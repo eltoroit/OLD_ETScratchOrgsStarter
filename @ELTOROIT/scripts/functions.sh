d761915feeed3d088efec3197fcd32bcf69b378b
Andres Perez <andresperezf@yahoo.com>
2020-02-16 12:24:16 +0100

    Batch code cleaned, for reusability

--- @ELTOROIT/scripts/functions.sh
+++ @ELTOROIT/scripts/functions.sh
@@ -44,6 +44,75 @@ function et_sfdx(){
 function jq_sfdx(){
 	echo "\033[2;30msfdx $*\033[0m"
 	local sfdxResult=`sfdx $* || QuitError`
-	echo $sfdxResult | jq "del(.result.tests, .result.coverage)"
 }
 
+function everything() {
+	# ---
+	showStatus "*** Creating scratch Org..."
+	et_sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias "$ALIAS" -d "$DAYS"
+	showComplete
+
+	# ---
+	if [[ "$PAUSE2CHECK_ORG" = true ]]; then
+		showStatus "*** Opening scratch Org..."
+		et_sfdx force:org:open
+		showPause "Stop to validate the ORG was created succesfully."
+	fi
+
+	# ---
+	if [[ ! -z "$PATH2SETUP_METADATA_BEFORE" ]]; then
+		et_sfdx force:org:open --path "$PATH2SETUP_METADATA_BEFORE"
+		showPause "Configure additonal metadata BEFORE pushing"
+	fi
+
+	# ---
+	showStatus "*** Pushing metadata to scratch Org..."
+	et_sfdx force:source:push --json
+	showComplete
+
+	if [[ ! -z "$PATH2SETUP_METADATA_AFTER" ]]; then
+		et_sfdx force:org:open --path "$PATH2SETUP_METADATA_AFTER"
+		showPause "Configure additonal metadata AFTER pushing"
+	fi
+
+	# ---
+	if [ ! -z "$PERM_SET" ]
+	then
+		showStatus "*** Assigning permission set to your user..."
+		et_sfdx force:user:permset:assign --permsetname "$PERM_SET" --json
+		showComplete
+	fi
+
+	# ---
+	if [ ! -z "$EXEC_ANON_APEX" ]; then
+		showStatus "*** Execute Anonymous Apex..."
+		et_sfdx force:apex:execute -f "$EXEC_ANON_APEX"
+		showComplete
+	fi
+
+	# ---
+	if [[ "$IMPORT_DATA" = true ]]; then
+		showStatus "*** Creating data using ETCopyData plugin"
+		# et_sfdx ETCopyData:export -c "./@ELTOROIT/data" --loglevel warn --json
+		et_sfdx ETCopyData:import -c "./@ELTOROIT/data" --loglevel warn --json
+		showComplete
+	fi
+
+	# ---
+	if [[ "$RUN_APEX_TESTS" = true ]]; then
+		showStatus "Runing Apex tests"
+		jq_sfdx force:apex:test:run --codecoverage --synchronous --verbose --json --resultformat json
+		echo $sfdxResult | jq "del(.result.tests, .result.coverage)"
+		showComplete
+	fi
+
+	# ---
+	if [[ "$GENERATE_PASSWORD" = true ]]; then
+		showStatus "*** Generate Password..."
+		et_sfdx force:user:password:generate --json
+		et_sfdx force:user:display
+		showComplete
+	fi
+
+	QuitSuccess
+}
