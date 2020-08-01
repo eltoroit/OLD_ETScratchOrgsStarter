ead0c5749bf5ccf276f1ab6a31ec91e8e1304190
Andres Perez <aperez@salesforce.com>
2020-08-01 13:18:46 -0400

    Scripts updated, prepare for CI/CD

--- @ELTOROIT/scripts/shell/CreateOrg.sh
+++ @ELTOROIT/scripts/shell/CreateOrg.sh
@@ -1,9 +1,8 @@
 # Execute in Mac using: ./@ELTOROIT/scripts/CreateOrg.sh
 
-# --- Include helper scripts 
+# --- Define local folder
 	DIR="${BASH_SOURCE%/*}"
 	if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
-	source "$DIR/Functions.sh" "$DIR/Functions.sh"
 
 # --- Batch variables
 	# Alias for scratch org
@@ -14,7 +13,7 @@
 	
 	# Install required packages before pushing
 	# Sample: PACKAGES=("04tB0000000P1yA" "04tB0000000P1yB" "04tB0000000P1yC")
-	# PACKAGES=("04tB0000000P1yS")
+	# PACKAGES=("04tB0000000P1yA")
 
 	# Permission Set name
 	PERM_SET=psTest
@@ -22,7 +21,7 @@
 	# Path to Apex code to execute anonymously
 	# Sample: $DIR/AnonymousApex.txt
 	EXEC_ANON_APEX_BEFORE_PUSH=
-	EXEC_ANON_APEX_AFTER_PUSH=$DIR/SetDebugMode.apex
+	EXEC_ANON_APEX_AFTER_PUSH=$DIR/../apex/SetUserRecord.apex
 	EXEC_ANON_APEX_AFTER_DATA=
 	
 	# Is there any additional manual configuration required BEFORE pushing metadata?
@@ -34,21 +33,30 @@
 	PATH2SETUP_METADATA_AFTER=
 
 # --- Batch boolean variables
-	# Backup alias, in case you need to go back :-)
-	BACKUP_ALIAS=true
-
-	# Deploy Admin standard profile
-	# Helps set the page layouts, for example
-	DEPLOY_ADMIN=true
 
 	# Quit on errors?
 	QUIT_ON_ERRORS=true
 
 	# Automated Script? Can I prompt the user for a key to continue?
-	USER_ON_SCREEN=false
+	USER_ON_SCREEN=true
+
+	# Open browser page to show deployment progress
+	SHOW_DEPLOY_PAGE=true
+
+	# Do you want to run JEST tests in this project?
+	RUN_JEST_TESTS=true
 
 	# Stop to validate org was succesfully created? Sometimes Sslesforce fails when creating an org and shows the login screen rather than opening an org.
 	PAUSE2CHECK_ORG=false
+
+	# Backup alias, in case you need to go back :-)
+	BACKUP_ALIAS=true
+
+	# Deploy code before pushing it? I have seen some issues with deployments of communities that get solved like this
+	PERFORM_DEPLOY=true
+
+	# Deploy Admin standard profile (helps set the visible apprlications, for example)
+	DEPLOY_ADMIN=true
 
 	# Do you want to use ETCopyData to import data?
 	IMPORT_DATA=true
@@ -56,14 +64,8 @@
 	# Do you want to run APEX tests in this new org before starting?
 	RUN_APEX_TESTS=true
 
-	# Do you want to run JEST tests in this project?
-	RUN_JEST_TESTS=true
-
 	# Do you need a password for the user name? You may need this depening on the use of the new scratch org
 	GENERATE_PASSWORD=true
 
-	# Open browser page to show deployment progress
-	SHOW_DEPLOY_PAGE=true
-
 # --- Ready, set, go!
-	everything
+	source "$DIR/Functions.sh"
--- @ELTOROIT/scripts/shell/Functions.sh
+++ @ELTOROIT/scripts/shell/Functions.sh
@@ -2,36 +2,39 @@
 # 	https://www.shellhacks.com/bash-colors/
 # 	https://misc.flogisoft.com/bash/tip_colors_and_formatting
 
+function promptUser() {
+	printf "\033[0m"
+	read -e answer
+	printf "\n"	
+}
 function showStatus() {
 	# Magenta
 	printf "\033[0;35m$1\033[0m\n"
 }
-
+function showCommand() {
+	printf "\033[0;33m$*\033[0m\n"
+}
 function showComplete() {
 	# Green
 	printf "\033[0;32mTask Completed\033[0m\n"
 }
-
 function ReportError() {
 	printf "\033[0;31m\n"
 	printf "Org could not be created!\n"
 	if [[ "$QUIT_ON_ERRORS" = true ]]; then
 		if [[ "$USER_ON_SCREEN" = true ]]; then 
 			printf "Press [Enter] key to exit process..."
-			printf "\033[0m"	
-			read -p " "
+			promptUser
 			exit 1
 		else
-			printf "\033[0m\n"	
+			printf "\033[0m"	
 			exit 1
 		fi
 	else
 		printf "Press [Enter] key to continue..."
-		printf "\033[0m"	
-		read -p " "
+		promptUser
 	fi
 }
-
 function showPause(){
 	# This will fail on an automated process, but not sure how to fix it.
 	# Should I ask? Should I skip? Should I error?
@@ -40,8 +43,7 @@ function showPause(){
 		printf "%s " $*
 		printf "\n"
 		printf "Press [Enter] key to continue...  "
-		printf "\033[0m"
-		read -p " "
+		promptUser
 	else
 		printf "\033[0;31m"
 		printf "Automated tests! Should not be prompting for this"
@@ -49,7 +51,6 @@ function showPause(){
 		ReportError
 	fi
 }
-
 function QuitSuccess() {
 	# Green
 	printf "\033[0;32m\n"
@@ -59,118 +60,160 @@ function QuitSuccess() {
 	printf "\033[0m\n"
 	exit 0
 }
-
 function et_sfdx(){
-	printf "\033[2;30msfdx $*\033[0m\n"
+	showCommand "sfdx $*"
 	sfdx $* || ReportError
 }
-
 function et_sfdxPush(){
-	printf "\033[2;30msfdx $*\033[0m\n"
-	sfdx $* > etLogs/push.json || ReportError
+	showCommand "sfdx $*"
+	etFile=etLogs/push.json
+	sfdx $* >> $etFile
+	local resultcode=$?
+	if [[ "$AUTOMATED_PROCESS" = true ]]; then 
+		cat $etFile
+	fi
+	if [[ $resultcode -ne 0 ]]; then
+		ReportError
+	fi
 }
-
+function et_sfdxDeploy(){
+	showStatus "*** Deploying metadata to scratch Org..."
+	showCommand "sfdx $*"
+	etFile=etLogs/deploy.json
+	sfdx $* >> $etFile
+	local resultcode=$?
+	if [[ "$AUTOMATED_PROCESS" = true ]]; then 
+		cat $etFile
+	fi
+	if [[ $resultcode -ne 0 ]]; then
+		ReportError
+	fi
+}
 function jq_sfdxGeneratePassword() {
 	# Display has a bug where the password is encripted on display and can't be shown. 
 	# et_sfdx force:user:password:generate --json
 
-	sfdx force:user:password:generate --json > etLogs/tempPW.json
-	sfdx force:user:display --json > etLogs/userInfo.json
-	pwd=$(jq -r '.result.password' etLogs/tempPW.json)
-	jq --arg pwd "$pwd" '.result.password = $pwd' etLogs/userInfo.json > etLogs/user.tmp && mv etLogs/user.tmp etLogs/userInfo.json
-	rm etLogs/tempPW.json
-	cat etLogs/userInfo.json | jq
+	showCommand "Generates Password"
+	etFile=etLogs/userInfo.json
+	etFileTemp=$etFile.tmp
+	sfdx force:user:password:generate --json > $etFileTemp
+	sfdx force:user:display --json > $etFile
+	pwd=$(jq -r '.result.password' $etFileTemp)
+	jq --arg pwd "$pwd" '.result.password = $pwd' $etFile > $etFileTemp && mv $etFileTemp $etFile
+	cat $etFile | jq .
 }
-
-function jq_sfdxRunApexTests(){
-	printf "\033[2;30msfdx $*\033[0m\n"
-	sfdx $* > etLogs/apexTests.json
+function jq_sfdxRunApexTests() {
+	showCommand "sfdx $*"
+	etFile=etLogs/apexTests.json
+	sfdx $* >> $etFile
 	local resultcode=$?
-	cat etLogs/apexTests.json | jq "del(.result.tests, .result.coverage)"
 	if [[ $resultcode -ne 0 ]]; then
+		if [[ "$AUTOMATED_PROCESS" = true ]]; then 
+			cat $etFile
+		else 
+			cat $etFile | jq "del(.result.tests, .result.coverage)"
+		fi
 		printf "\033[0;31m\n"
 		printf "Tests run, but they failed!\n"
 		if [[ "$QUIT_ON_ERRORS" = true ]]; then
 			if [[ "$USER_ON_SCREEN" = true ]]; then 
 				printf "Press [Enter] key to exit process...  "
-				printf "\033[0m"
-				read -p " "
+				promptUser
 			fi
 			exit 1
 		fi
 		if [[ "$USER_ON_SCREEN" = true ]]; then 
 			printf "Press [Enter] key to continue...  "	
-			printf "\033[0m"
-			read -p " "
+			promptUser
 		fi
+	else
+		cat $etFile | jq "del(.result.tests, .result.coverage)"
 	fi
 }
-
 function backupAlias() {
-	printf "\033[2;30msfdx force:alias:list --json\033[0m\n"
-	sfdx force:alias:list --json > etLogs/aliasList.json
+	showCommand "sfdx force:alias:list --json"
+	etFile=etLogs/aliasList.json
+	sfdx force:alias:list --json >> $etFile
+	if [[ "$AUTOMATED_PROCESS" = true ]]; then 
+		cat $etFile
+	fi
 
-	printf "\033[2;30mFinding Username for $ALIAS\033[0m\n"
-	cat etLogs/aliasList.json | jq --arg JQALIAS "$ALIAS" '.result[] | select(.alias==$JQALIAS) | .value' | while read -r UN; do
+	showCommand "Finding Username for $ALIAS"
+	cat $etFile | jq --arg JQALIAS "$ALIAS" '.result[] | select(.alias==$JQALIAS) | .value' | while read -r UN; do
 		local TEMP="${UN%\"}"
 		UN="${TEMP#\"}"
-		# printf "\033[2;30m[$ALIAS.bak] <= [$UN]\033[0m\n"
+		# showCommand "[$ALIAS.bak] <= [$UN]"
 		et_sfdx force:alias:set $ALIAS.bak=$UN
 	done
 }
 
-function everything() {
-	DEPLOY_PAGE="/lightning/setup/DeployStatus/home"
 
-# --- Run JEST tests before anything else!
+
+function mainRunJest() {
+	# --- Run JEST tests before anything else!
 	if [[ "$RUN_JEST_TESTS" = true ]]; then
 		showStatus "*** Running JEST tests..."
-		JEST_LOG=etLogs/jestTest.json
-		printf "\033[2;30mJEST logs are here: $JEST_LOG\033[0m\n"
-		npm run test:unit:coverage &> $JEST_LOG || ReportError
-		showComplete
-	fi
-
-# --- Backup this org alias
+		etFile=etLogs/jestTests.json
+		showCommand "JEST logs are here: $etFile"
+		npm run test:unit:coverage &> $etFile
+		local resultcode=$?
+		if [[ $resultcode -ne 0 ]]; then
+			if [[ "$AUTOMATED_PROCESS" = true ]]; then 
+				cat $JEST_LOG
+			fi
+	 		ReportError
+		else
+			showComplete
+		fi
+	fi	
+}
+function mainBackupAlias() {
+	# --- Backup this org alias
 	if [[ "$BACKUP_ALIAS" = true ]]; then
 		showStatus "*** Backup this org alias..."
 		backupAlias
 		showComplete
 	fi
-
-# --- Create scratch org
+}
+function mainCreateScratchOrg() {
+	# --- Create scratch org
 	showStatus "*** Creating scratch Org..."
 	et_sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias "$ALIAS" -d "$DAYS"
 	showComplete
-
-# --- Pause to valiate org created
+}
+function mainPauseToCheck() {
+	# --- Pause to valiate org created
 	if [[ "$PAUSE2CHECK_ORG" = true ]]; then
 		showStatus "*** Opening scratch Org..."
 		et_sfdx force:org:open
 		showPause "Stop to validate the ORG was created succesfully."
 	fi
-
-# --- Open deploy page to watch deployments
+}
+function mainOpenDeployPage() {
+	# --- Open deploy page to watch deployments
 	if [[ "$SHOW_DEPLOY_PAGE" = true ]]; then
 		showStatus "*** Open page to monitor deployment..."
 		et_sfdx force:org:open --path=$DEPLOY_PAGE
 	fi
-
-# --- Manual metadata (before deployment)
+}
+function mainManualMetadataBefore() {
+	# --- Manual metadata (before deployment)
 	if [[ ! -z "$PATH2SETUP_METADATA_BEFORE" ]]; then
 		showStatus "*** Open page to configure org (BEFORE pushing)..."
 		et_sfdx force:org:open --path "$PATH2SETUP_METADATA_BEFORE"
 		showPause "Configure additonal metadata BEFORE pushing..."
 	fi
-
-# --- Execute Apex Anonymous code (Before Push)
+}
+function mainExecuteApexBeforePush() {
+	# --- Execute Apex Anonymous code (Before Push)
 	if [ ! -z "$EXEC_ANON_APEX_BEFORE_PUSH" ]; then
 		showStatus "*** Execute Anonymous Apex (before push)..."
 		et_sfdx force:apex:execute -f "$EXEC_ANON_APEX_BEFORE_PUSH"
 		showComplete
 	fi
-
-# --- Install Packages (Before Push)
+}
+function mainInstallPackages() {
+	# --- Install Packages (Before Push)
 	if [ ! -z "$PACKAGES" ]; then
 		showStatus "*** Installing Packages (before push)..."
 		for PACKAGE in ${PACKAGES[@]}; do
@@ -181,76 +224,172 @@ function everything() {
 		done
 		showComplete
 	fi
-
-# --- Push metadata
+}
+function mainDeploy() {
+	if [[ "$PERFORM_DEPLOY" = true ]]; then
+		jq '.packageDirectories[].path' sfdx-project.json > etLogs/tmpDeploy.txt
+		while read -r path <&9; do
+			folder=$(echo $path | tr -d '"')
+			et_sfdxDeploy force:source:deploy --sourcepath "./$folder" --json --loglevel fatal
+		done 9<  etLogs/tmpDeploy.txt
+		rm etLogs/tmpDeploy.txt
+	fi
+}
+function mainPushMetadata() {
+	# --- Push metadata
 	showStatus "*** Pushing metadata to scratch Org..."
 	# if [[ "$SHOW_DEPLOY_PAGE" = true ]]; then
 	# 	et_sfdx force:org:open --path=$DEPLOY_PAGE
 	# fi
-	et_sfdxPush force:source:push
+	et_sfdxPush force:source:push --forceoverwrite --json
 	showComplete
-
-# --- Execute Apex Anonymous code (After Push)
+}
+function mainExecuteApexAfterPush() {
+	# --- Execute Apex Anonymous code (After Push)
 	if [ ! -z "$EXEC_ANON_APEX_AFTER_PUSH" ]; then
 		showStatus "*** Execute Anonymous Apex (after push)..."
 		et_sfdx force:apex:execute -f "$EXEC_ANON_APEX_AFTER_PUSH"
 		showComplete
 	fi
-
-# --- Manual metadata (after deployment)
+}
+function mainManualMetadataAfter() {
+	# --- Manual metadata (after deployment)
 	if [[ ! -z "$PATH2SETUP_METADATA_AFTER" ]]; then
 		showStatus "*** Open page to configure org (AFTER pushing)..."
 		et_sfdx force:org:open --path "$PATH2SETUP_METADATA_AFTER"
 		showPause "Configure additonal metadata AFTER pushing..."
 	fi
-
-# --- Assign permission set
+}
+function mainAssignPermissionSet() {
+	# --- Assign permission set
 	if [ ! -z "$PERM_SET" ]
 	then
 		showStatus "*** Assigning permission set to your user..."
 		et_sfdx force:user:permset:assign --permsetname "$PERM_SET" --json
 		showComplete
 	fi
-
-# --- Deploy profile (Maybe .forceignore prevents them from push/pull)
+}
+function mainDeplyAdminProfile() {
+	# --- Deploy profile
 	ADMIN_PROFILE=./doNotDeploy/main/default/profiles/Admin.profile-meta.xml
 	if [ ! -z "$ADMIN_PROFILE" ]; then
 		if [[ "$DEPLOY_ADMIN" = true ]]; then
 			showStatus "*** Deploying 'Admin' standard profile..."
-			mv .forceignore .forceignore.old
+			mv .forceignore etLogs/.forceignore
 			et_sfdx force:source:deploy -p "$ADMIN_PROFILE"
-			mv .forceignore.old .forceignore
+			local resultcode=$?
+			mv etLogs/.forceignore .forceignore
+			showComplete
 		fi
 	fi
-
-# --- Load data using ETCopyData plugin
+}
+function mainLoadData() {
+	# --- Load data using ETCopyData plugin
 	if [[ "$IMPORT_DATA" = true ]]; then
 		showStatus "*** Creating data using ETCopyData plugin..."
 		# et_sfdx ETCopyData:export -c "./@ELTOROIT/data" --loglevel warn --json
 		et_sfdx ETCopyData:import -c "./@ELTOROIT/data" --loglevel warn --json
 		showComplete
 	fi
-
-# --- Execute Apex Anonymous code
+}
+function mainExecuteApexAfterData() {
+	# --- Execute Apex Anonymous code after data
 	if [ ! -z "$EXEC_ANON_APEX_AFTER_DATA" ]; then
 		showStatus "*** Execute Anonymous Apex..."
 		et_sfdx force:apex:execute -f "$EXEC_ANON_APEX_AFTER_DATA"
 		showComplete
 	fi
-
-# --- Generate Password
+}
+function mainGeneratePassword() {
+	# --- Generate Password
 	if [[ "$GENERATE_PASSWORD" = true ]]; then
 		showStatus "*** Generate Password..."
 		jq_sfdxGeneratePassword
 		showComplete
 	fi
-
-# --- Runing Apex tests
+}
+function mainRunApexTests() {
+	# --- Runing Apex tests
 	if [[ "$RUN_APEX_TESTS" = true ]]; then
 		showStatus "Runing Apex tests..."
 		jq_sfdxRunApexTests force:apex:test:run --codecoverage --synchronous --verbose --json --resultformat json
 		showComplete
 	fi
-
-	QuitSuccess
 }
+function mainDisplayAlias() {
+	# Used for testing the script is called in GitHub Actions
+	echo "ALIAS: $ALIAS"
+}
+function mainPushAgain() {
+	# --- Push metadata
+	showStatus "*** Pushing metadata to scratch Org one more time..."
+	# if [[ "$SHOW_DEPLOY_PAGE" = true ]]; then
+	# 	et_sfdx force:org:open --path=$DEPLOY_PAGE
+	# fi
+	et_sfdxPush force:source:push -u "$ALIAS" -f
+	showComplete
+}
+function mainFinalSetup() {
+	# --- Push metadata
+	showStatus "*** Final touches..."
+	et_sfdx force:config:set defaultusername=$ALIAS
+	showComplete
+}
+
+DEPLOY_PAGE="/lightning/setup/DeployStatus/home"
+rm -rf ./etLogs
+mkdir -p etLogs
+AUTOMATED_PROCESS=false
+if [[ "$ET_CICD" = true ]]; then
+	AUTOMATED_PROCESS=true
+fi
+echo "ET_CICD: $ET_CICD"
+echo "AUTOMATED_PROCESS: $AUTOMATED_PROCESS"
+
+if [[ ! -z "$*" ]]; then
+	$*
+else
+	# No parameters sent, then do everything
+	mainRunJest
+	mainBackupAlias
+	mainCreateScratchOrg
+	mainPauseToCheck
+	mainOpenDeployPage
+	mainManualMetadataBefore
+	mainExecuteApexBeforePush
+	mainInstallPackages
+	mainDeploy
+	mainPushMetadata
+	mainExecuteApexAfterPush
+	mainManualMetadataAfter
+	mainAssignPermissionSet
+	mainDeplyAdminProfile
+	mainLoadData
+	mainExecuteApexAfterData
+	mainRunApexTests
+	mainPushAgain
+	mainFinalSetup
+	mainGeneratePassword
+	QuitSuccess
+fi
+
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainRunJest
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainBackupAlias
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainCreateScratchOrg
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainPauseToCheck
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainOpenDeployPage
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainManualMetadataBefore
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainExecuteApexBeforePush
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainInstallPackages
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainPushMetadata
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainExecuteApexAfterPush
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainManualMetadataAfter
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainAssignPermissionSet
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainDeplyAdminProfile
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainLoadData
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainExecuteApexAfterData
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainRunApexTests
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainPushAgain
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainFinalSetup
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh mainGeneratePassword
+# ./@ELTOROIT/scripts/shell/CreateOrg.sh QuitSuccess
